0c10865238ab2951879cc57369a2dce1d5c1e00e
Vancho Stojkov <408278+van100j@users.noreply.github.com>
2025-02-04 16:33:18 +0100

    Add onProgress example

--- apps/web/examples/PDFViewerApp/index.tsx
+++ apps/web/examples/PDFViewerApp/index.tsx
@@ -9,13 +9,18 @@ type PDFViewerAppProps = {
 
 export default function PDFViewerApp({ pdfFilePath }: PDFViewerAppProps) {
   const [isThumbsbarOpen, setIsThumbsbarOpen] = useState(false);
+  const [loadedPerc, setLoadedPerc] = useState(0);
   const {
     isDocumentLoaded,
     viewerRef,
     thumbsRef,
     usePDFSlickStore,
     PDFSlickViewer,
-  } = usePDFSlick(pdfFilePath);
+  } = usePDFSlick(pdfFilePath, {
+    onProgress: ({ total, loaded }) => {
+      setLoadedPerc((100 * loaded) / total);
+    },
+  });
 
   useEffect(() => {
     if (isDocumentLoaded) {
@@ -24,15 +29,25 @@ export default function PDFViewerApp({ pdfFilePath }: PDFViewerAppProps) {
   }, [isDocumentLoaded]);
 
   return (
-    <div className="absolute inset-0 bg-slate-200/70 flex flex-col pdfSlick">
-      <Toolbar {...{ usePDFSlickStore, setIsThumbsbarOpen, isThumbsbarOpen }} />
-      <div className="flex-1 flex">
-        <Thumbsbar {...{ thumbsRef, usePDFSlickStore, isThumbsbarOpen }} />
+    <>
+      <div className="absolute inset-0 bg-slate-200/70 flex flex-col pdfSlick">
+        <Toolbar
+          {...{ usePDFSlickStore, setIsThumbsbarOpen, isThumbsbarOpen }}
+        />
+        <div className="flex-1 flex">
+          <Thumbsbar {...{ thumbsRef, usePDFSlickStore, isThumbsbarOpen }} />
 
-        <div className="flex-1 relative h-full">
-          <PDFSlickViewer {...{ viewerRef, usePDFSlickStore }} />
+          <div className="flex-1 relative h-full">
+            <PDFSlickViewer {...{ viewerRef, usePDFSlickStore }} />
+          </div>
         </div>
       </div>
-    </div>
+      {loadedPerc < 100 && (
+        <div
+          className="fixed top-0 left-0 h-px bg-blue-600 z-50 transition-all duration-150 ease-out"
+          style={{ width: `${loadedPerc}%` }}
+        ></div>
+      )}
+    </>
   );
 }
