93adb0a0e271f5a1441d1bb0238a98aef304e1a8
Vancho Stojkov <408278+van100j@users.noreply.github.com>
2025-11-17 10:40:51 +0100

    Update styles to fix dark mode

--- apps/web/examples/PDFViewerApp/Toolbar/index.tsx
+++ apps/web/examples/PDFViewerApp/Toolbar/index.tsx
@@ -63,7 +63,7 @@ const Toolbar = ({
   return (
     <>
       <div
-        className={`w-full h-9 flex items-center justify-between bg-slate-50 border-b border-b-slate-300 shadow-sm text-xs select-none sticky top-0 backdrop-blur z-10`}
+        className={`w-full text-slate-500 h-9 flex items-center justify-between bg-slate-50 border-b border-b-slate-300 shadow-sm text-xs select-none sticky top-0 backdrop-blur z-10`}
       >
         <div className="px-1 flex items-center space-x-1">
           <button
