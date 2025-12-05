a2f3e50790dcbe9215ffdd192f5ead49886a6345
Gijs Verdonk <g.verdonk@icloud.com>
2025-12-05 11:07:20 +0100

    Add Comments Example.

---
+++ apps/web/examples/Comments/index.tsx
@@ -0,0 +1,3 @@
+export default function CommentsExample() {
+  return <div>This is the PDFViewer with comments example</div>;
+}
---
+++ apps/web/pages/examples/comments.tsx
@@ -0,0 +1,10 @@
+import CommentsExample from "../../examples/Comments";
+
+export default function Comments() {
+  return (
+  <div>
+    <h1>Comments Example</h1>
+    <CommentsExample/>
+  </div>
+  );
+}
