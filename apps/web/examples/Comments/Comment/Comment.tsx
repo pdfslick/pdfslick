d66d44d29a5997ba2787fa71785b6bb58bf7cc4b
Gijs Verdonk <g.verdonk@icloud.com>
2025-12-10 14:35:54 +0100

    Add HandleSubmit.

--- apps/web/examples/Comments/Comment/Comment.tsx
+++ apps/web/examples/Comments/Comment/Comment.tsx
@@ -8,9 +8,15 @@ export default function Comment() {
     function handleClose() {
         setIsOpen(false);
     }
+
+    function handleSubmit() {
+        console.log("The following comment was submitted: ", comment);
+        setComment("");
+        handleClose();
+    }
     
   return (
-    <div className={`fixed left-4 top-4 bg-slate-800 z-10 p-3 rounded shadow-lg ${
+    <div className={`fixed left-4 top-4 bg-gray-800 z-10 p-3 rounded shadow-lg ${
         isOpen ? 'visible' : 'invisible'
       }`}>
         <div className="flex justify-between items-center">
@@ -19,7 +25,7 @@ export default function Comment() {
         </div>
         <div className="flex flex-col">
             <input type="text" className="border border-slate-300 rounded p-2" value={comment} onChange={(e) => setComment(e.target.value)} />
-            <button className="bg-indigo-600 hover:bg-indigo-500 text-white mt-2 rounded" onClick={handleClose}>Submit</button>
+            <button className="bg-gray-600 hover:bg-gray-500 text-white mt-2 rounded" onClick={handleSubmit}>Submit</button>
         </div>
   </div>
   )
