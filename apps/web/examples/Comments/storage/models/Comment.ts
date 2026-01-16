2d69a398c2ca5cf156bd5a9ab66fbe4a0b03d4b0
Margarita <i557795@fontysict.nl>
2026-01-16 11:35:09 +0100

    Finish the migrating by changing PinButton and Comment model

--- apps/web/examples/Comments/storage/models/Comment.ts
+++ apps/web/examples/Comments/storage/models/Comment.ts
@@ -1,7 +1,7 @@
 export interface Comment {
     comment_id: string;
     annotation_id: string;
-    user_name: string;
+    user_id: string;
     contents: string;
     created_at: string;
     updated_at: string;
